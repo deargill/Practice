3ffee1b5702d326593da01cfd965c3087e6d8c2f
deargill <henrysmitty9@gmail.com>
2023-03-10 12:28:41 +0300

    PR4
    Practice 3

---
+++ PR4/ZAD2/Class/TwoDimensionalArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD2
+{
+    public class TwoDimensionalArray
+    {
+        private int[,] array;
+
+        public TwoDimensionalArray(int[,] array)
+        {
+            this.array = array;
+        }
+
+        public int GetMultOfPosElLessThTen()
+        {
+            int product = 1;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > 0 && array[i, j] < 10)
+                    {
+                        product *= array[i, j];
+                    }
+                }
+            }
+            return product;
+        }
+    }
+}
---
+++ PR4/ZAD2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+            TwoDimensionalArray tda = new TwoDimensionalArray(myArray);
+            int product = tda.GetMultOfPosElLessThTen();
+            Console.WriteLine(product);
+        }
+    }
+}
