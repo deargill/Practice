edfce9c90f2b21d8a964f888a7adf30a7a9ed6d4
deargill <henrysmitty9@gmail.com>
2023-04-06 18:07:39 +0300

    Practice 15

---
+++ PR16/ZAD2/Program.cs
@@ -0,0 +1,9 @@
+﻿using MyLibraryClasses;
+
+MyDictionary<int, string> dict = new MyDictionary<int, string>();
+dict.Add(1, "Макс");
+dict.Add(2, "Брэд");
+dict.Add(3, "Баран");
+
+Console.WriteLine($"Количество: {dict.Counter}");
+Console.WriteLine(dict.GetElem(3));
