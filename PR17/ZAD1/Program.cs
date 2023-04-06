8144a347ac0eb0bc1dfa7788884d4a2bb59196ba
deargill <henrysmitty9@gmail.com>
2023-04-06 18:31:32 +0300

    Practice 16

---
+++ PR17/ZAD1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string fileName = "f.txt";
+
+        try
+        {
+            double[] numbers = File.ReadAllLines(fileName)
+                                    .Select(line => double.Parse(line))
+                                    .ToArray();
+
+            double maxValue = numbers.Max();
+            double minValue = numbers.Min();
+
+            double sum = maxValue + minValue;
+
+            // Вывод результата
+            Console.WriteLine($"Сумма наибольшего ({maxValue}) и наименьшего ({minValue}) значения: {sum}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Ошибка: {e.Message}");
+        }
+    }
+}
---
+++ PR17/ZAD2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"C:\New_folder";
+        Directory.CreateDirectory(path);
+    }
+}
