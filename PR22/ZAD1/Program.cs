da40bb1071fe9b5b3494d0399d03a03e2f45e99b
deargill <henrysmitty9@gmail.com>
2023-04-08 04:14:51 +0300

    Practice 21

---
+++ PR22/ZAD1/Program.cs
@@ -0,0 +1,10 @@
+﻿using MyLibraryClass;
+
+CoffeeMachine coffeeMachine = CoffeeMachine.getState();
+
+coffeeMachine.addWater();
+coffeeMachine.addCoffeeBeans();
+
+coffeeMachine.makeCoffee();
+
+coffeeMachine.coffeeMachineState();
