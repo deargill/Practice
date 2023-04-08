84e739c44f47e191a1ce1c2d5b79f3e4f7f11d2b
deargill <henrysmitty9@gmail.com>
2023-04-08 04:13:33 +0300

    Practice 18

---
+++ PR19/ZAD3/Class/Prototype.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibraryClass
+{
+    interface Prototype
+    {
+        object DeepCopy();
+        Prototype Clone();
+        public void GetInfo();
+        public void AddFish(string fish);
+    }
+}
---
+++ PR19/ZAD3/Program.cs
@@ -0,0 +1,9 @@
+﻿using MyLibraryClass;
+
+List<string> fishes_1 = new List<string>() { "Нэмо", "Форель" };
+Sea sea_1 = new Sea("Адриатическое", 1233, fishes_1);
+
+Sea sea_2 = sea_1.DeepCopy() as Sea;
+sea_2.AddFish("Дори");
+sea_1.GetInfo();
+sea_2.GetInfo();
